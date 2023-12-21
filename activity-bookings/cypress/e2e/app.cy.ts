221d8dd4310bc3a4d86036f9995c2c9346185a91
Alberto Basalo <albertobasalo@hotmail.com>
2023-12-21 17:39:55 +0100

    test: e2e header and footer

--- activity-bookings/cypress/e2e/app.cy.ts
+++ activity-bookings/cypress/e2e/app.cy.ts
@@ -1,13 +1,26 @@
-import { getGreeting } from '../support/app.po';
-
 describe('activity-bookings', () => {
   beforeEach(() => cy.visit('/'));
 
   it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+    cy.get('h1').contains(/Activity Bookings/);
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains(/Activity/);
+  it('should display header with title (Activity Bookings), home link (Home), user link (John Doe), activities link (Activities), and bookings link (Bookings)', () => {
+    cy.get('header')
+      .first()
+      .within(() => {
+        cy.get('h1').contains(/Activity Bookings/);
+        cy.get('a').contains(/Home/);
+        cy.get('a').contains(/John Doe/);
+        cy.get('a').contains(/Activities/);
+        cy.get('a').contains(/Bookings/);
+      });
+  });
+
+  it('should display footer with author (Alberto Basalo), solution (Angular 17 Nx), and year(2023)', () => {
+    cy.get('footer').contains(/Alberto Basalo/);
+    cy.get('footer').contains(/Angular 17 Nx/);
+    const currentYear = new Date().getFullYear();
+    cy.get('footer').contains(currentYear);
   });
 });
