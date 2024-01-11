eface4abd2d67b1ede94b231a6faa2cf6a671b95
Alberto Basalo <albertobasalo@hotmail.com>
2024-01-11 18:41:06 +0100

    docs: instrumentation and lint

--- activity-bookings/src/app/app.config.ts
+++ activity-bookings/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 
-import { provideInstrumentation, withMinLogLevel } from '@lab/core';
+import { provideInstrumentation, withOnlyErrors } from '@lab/core';
 
 import { environment } from '../environments/environment';
 import { appRoutes } from './app.routes';
@@ -12,7 +12,7 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideRouter(appRoutes),
     provideInstrumentation(
-      withMinLogLevel(environment.instrumentation.onlyErrors)
+      withOnlyErrors(environment.instrumentation.onlyErrors)
     ),
   ],
 };
--- core/src/lib/instrumentation.provider.ts
+++ core/src/lib/instrumentation.provider.ts
@@ -12,6 +12,11 @@ import {
   MIN_LOG_LEVEL,
 } from '@lab/log';
 
+/**
+ * Factory funcion that returns providers for the instrumentation lib.
+ * @param minLogLevel
+ * @returns
+ */
 export const provideInstrumentation = (minLogLevel: MinLogLevelProvider) => {
   const providers: Provider[] = [
     {
@@ -34,9 +39,14 @@ type MinLogLevelProvider = {
   useValue: LogLevel;
 };
 
-type WithMinLogLevel = (onlyErrors: boolean) => MinLogLevelProvider;
+type WithOnlyErrors = (onlyErrors: boolean) => MinLogLevelProvider;
 
-export const withMinLogLevel: WithMinLogLevel = (onlyErrors: boolean) => {
+/**
+ * Factory function that returns a provider for the min log level token.
+ * @param onlyErrors Boolean indicating if only errors should be logged.
+ * @returns
+ */
+export const withOnlyErrors: WithOnlyErrors = (onlyErrors: boolean) => {
   return {
     provide: MIN_LOG_LEVEL,
     useValue: onlyErrors ? LogLevel.error : LogLevel.debug,
